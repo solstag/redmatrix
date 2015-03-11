9d61ec9d4d1fa650652a99bdfc2095b718388032
Alexandre Hannud Abdo <abdo@member.fsf.org>
2015-03-11 00:51:07 -0300

    fixes

--- view/php/sequence.php
+++ view/php/sequence.php
@@ -2,20 +2,24 @@
 <html>
 <head>
   <title><?php if(x($page,'title')) echo $page['title'] ?></title>
+  <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <!-- script src="//code.jquery.com/jquery-1.10.2.js"></script -->
   <script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
   <script>
   var baseurl="<?php echo $a->get_baseurl() ?>";
-  var pagename=argv(2);
+  var fullname="<?php echo argv(2) ?>";
   $(function() {
     $( "#accordion" ).accordion({
-      active: $("#menu-header-"+pagename.split('-',1)[0]).index();
+      active: $("#menu-header-"+fullname.split('-',1)[0]).index()/2,
+      heightStyle: "content"
     });
+    var activeid="#menu-header-"+fullname;
+    $(activeid).addClass("menu-item-active");
+    $(activeid+" a").click(function(e){e.preventDefault()});
+    $("#tabs").tabs();
   });
-  $("#menu-header-"+pagename).addClass("menu-item-active");
   </script>
-  <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
 </head>
 <body>
 	<header><?php if(x($page,'header')) echo $page['header']; ?></header>
@@ -26,26 +30,26 @@
 
 			<div id="tabs">
 				<ul>
-<?php
-for($i=1;;i++){
-	if(x($page,'sequence_'+$i)){
-		echo '<li><a href="#sequence-' . $i . '">$i</a></li>';
-	}
-	else break;
-}
-?>
+					<?php
+					for($i=1;;$i++){
+						if(x($page,'sequence_'+$i)){
+							echo '<li><a href="#sequence-' . $i . '">$i</a></li>';
+						}
+						else break;
+					}
+					?>
 				</ul>
-<?php
-if(x($page,'content')) echo $page['content'];
-for($i=1;;i++){
-	if(x($page,'sequence_'+$i)){
-		echo '<div id="sequence-' . $i . '">';
-		echo $page['sequence_' . $i];
-		echo '</div>';
-	}
-	else break;
-}
-?>
+				<?php
+				if(x($page,'content')) echo $page['content'];
+				for($i=1;;$i++){
+					if(x($page,'sequence_'+$i)){
+						echo '<div id="sequence-' . $i . '">';
+						echo $page['sequence_' . $i];
+						echo '</div>';
+					}
+					else break;
+				}
+				?>
 			</div>
 			<div id="page-footer"></div>
 			<div id="pause"></div>
