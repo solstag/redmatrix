ee3a4a5565ddb5f01d22b245c53df0ec1f8e194f
Alexandre Hannud Abdo <abdo@member.fsf.org>
2015-04-24 12:28:24 -0300

    Initial work on interactively enabling course steps

--- custom/course_widgets.php
+++ custom/course_widgets.php
@@ -2,7 +2,7 @@
 /**
  * @file custom/widgets.php
  *
- * @brief This file contains widgets to make course pages.
+ * @brief Widgets for course pages used togheter with the sequence template
  */
 
 function widget_coursetabs($arr){
@@ -35,13 +35,36 @@ function widget_coursetabs($arr){
 		$o .= prepare_text($rr['body'], $rr['mimetype']);
 		$o .= '</div>';
 	}
-	$o .= '</div>';
+	$o .= '<div class="sequence-buttons">'
+	$o .= '<button class="sequence-button sequence-button-previous" disabled="disabled">Voltar</button>'
+	$o .= '<button class="sequence-button sequence-button-next">Seguir</button>'
+	$o .= '</div>'
+
+/*	$k = array_keys($r);
+	for($i=0; $i < count($k); $i++){
+		$kk = $k[$i];
+		$o .= '<div id="' . $r[$kk]['sid'] . '">';
+		$o .= prepare_text($r[$kk]['body'], $r[$kk]['mimetype']);
+		$o .= '</div>';
+
+		$o .= '<div class="seqprevnext">';
+		$prevhref = (i==0) ? '' : $r[$k[$i-1]]['sid'];
+		$nexthref = (i==count($k)-1) ? '' : $r[$k[$i+1]]['sid'];
+		if($i!=0)
+			$o .= '<a href="#'. $prevhref .'" class="seqprev">Voltar</span>';
+		$o .= ' | ';
+		if($i!=count($k-1))
+			$o .= '<a href="#'. $nexthref .'" class="seqnext">Seguir</span>';
+		$o .= '</div>';
+	} */
+
+	$o .= '</div>'; // -seqtabs
 	return $o;
 }
 
 function widget_courserpost($arr){
 	$a = get_app();
-	$_SESSION['rpost'] = ["body"=>"Troque este texto pelo relato da sua experiência! Se preferir falar, anexe uma gravação de áudio sua, ou mesmo um vídeo.", "title"=>"Minha experiência no módulo " . $arr['name'], "remote_return"=>$a->get_baseurl() . $_SERVER[REQUEST_URI], "source"=>"Curso de Atenção Plena",];
+	$_SESSION['rpost'] = ["body"=>"Troque este texto pelo relato da sua experiência! Se preferir falar, anexe uma gravação de áudio sua, ou mesmo um vídeo.", "title"=>"Minha experiência no módulo " . $arr['name'], "remote_return"=>$a->get_baseurl() . $_SERVER[REQUEST_URI] . '#' . $arr['name'] . '-seq-z', "source"=>"Curso de Atenção Plena",];
 	require_once("mod/rpost.php");
 	$rpost_div = '<div id="rpost-data" style="display:none">' . rpost_content($a) . '</div>';
 	return $rpost_div;
--- view/php/sequence.php
+++ view/php/sequence.php
@@ -1,3 +1,11 @@
+<!--
+Building a page:
+*Menu entries must have ID "accmenu-$pagename"
+*Sequence tabs are blocks named "$pagename-seq-$tabname"
+*Tabs are ordered according to php comparison on $tabname strings
+*You must place a [widget=coursetabs][/widget] in your layout
+-->
+
 <!DOCTYPE html >
 <html>
 <head>
@@ -7,9 +15,16 @@
   <script src="//atencao-plena.rhcloud.com/custom/jquery-ui-1.11.4/jquery-ui.js"></script>
   <script>
   var baseurl="<?php echo $a->get_baseurl() ?>";
-  var fullname="<?php echo argv(2) ?>";
+  var pagename="<?php echo argv(2) ?>";
+  var flagname="<?php echo local_channel() ? get_pconfig(local_channel(),'system','startpage') : ''; ?>";
+  var flagseq='';
+  flagname = end(explode('/',flagname));
+  list(flagname,flagseq) = explode('#',flagname);
   var makeTabs = function(selector) {
-      $( selector )
+      var numtabs = $( selector ).find( " ul a" ).length;
+      var seqlastenabled = $( selector ).find("ul a[href="+flagname+"]").parent().index();
+
+      $( selector ) // change hrefs to full URL as "base href" is set
           .find( "ul a" ).each( function() {
               var href = $( this ).attr( "href" ),
                   newHref = window.location.protocol + '//' + window.location.hostname +
@@ -19,20 +34,51 @@
                   $( this ).attr( "href", newHref );
               }
           })
-      $( selector ).tabs();
+
+      if (seqlastenabled < 0) seqlastenabled = 0;
+      var seqdisabled = [];
+      for(var i=seqlastenabled+1; i<numtabs; i++){ seqenabled.push(i); }
+
+      $( selector ).tabs({show: true, disabled: seqdisabled});
+
+      $( "#rpost-data" ).appendTo( $("#" + pagename + "-seq-rpost") ).show();
+
+      var updateSeqButtons = function(index) {
+		  if(index==0)
+		      $(".sequence-button-previous").attr("disabled","disabled");
+		  else
+		      $(".sequence-button-previous").removeAttr("disabled");
+		  if(index==numtabs)
+		      $(".sequence-button-next").attr("disabled","disabled");
+		  else
+		      $(".sequence-button-next").removeAttr("disabled");
+      }
+      $( ".sequence-button-previous" ).click(function() {
+          var newindex = $( selector ).tabs( "option", "active") - 1 ;
+          $( selector ).tabs( "option", "active", newindex );
+          updateSeqButtons(newindex);
+      });
+      $( ".sequence-button-next" ).click(function() {
+          var newindex = $( selector ).tabs( "option", "active") + 1 ;
+          $( selector ).tabs( "enable", newindex );
+          $( selector ).tabs( "option", "active", newindex );
+          updateSeqButtons(newindex);
+      });
+  };
+  var makeMenu = function(activeitem, flagitem) {
+      var activeheader = $(activeitem).parent().prev().index()/2;
+      if (activeheader < 0) activeheader = 0;
+      $( "#accordion" ).accordion({
+          active: activeheader,
+          heightStyle: "content"
+      });
+      $(activeitem).addClass("menu-item-active");
+      $(flagitem).addClass("menu-item-flag");
+      $(activeitem+" a").click(function(e){e.preventDefault()});
   };
   $(function() {
-    var activeitem="#accmenu-"+fullname;
-    var activeheader = $(activeitem).parent().prev().index()/2;
-    if (activeheader < 0) activeheader = 0;
-    $( "#accordion" ).accordion({
-      active: activeheader,
-      heightStyle: "content"
-    });
-    $(activeitem).addClass("menu-item-active");
-    $(activeitem+" a").click(function(e){e.preventDefault()});
-    makeTabs( "#" + fullname + "-seqtabs" );
-    $( "#rpost-data" ).appendTo( $("#" + fullname + "-seq-rpost") ).show();
+      makeMenu( "#accmenu-" + pagename, "#accmenu-" + flagname );
+      makeTabs( "#" + pagename + "-seqtabs" );
   });
   </script>
 </head>
@@ -53,12 +99,4 @@
 	<footer><?php if(x($page,'footer')) echo $page['footer']; ?></footer>
 </body>
 </html>
-
-<!--
-Building a page:
-*Menu entries must have ID "accmenu-$pagename"
-*Sequence tabs are blocks named "$pagename-seq-$tabname"
-*Tabs are ordered according to php comparison on $tabname strings
-*You must place a [widget=coursetabs][/widget] in your layout
--->
 
