7b76046e65a7a9e30ebe9a45477f9d04dbec2f99
Alexandre Hannud Abdo <abdo@member.fsf.org>
2015-05-08 06:14:05 -0300

    enclose %s in q

--- custom/courses_widgets.php
+++ custom/courses_widgets.php
@@ -8,7 +8,7 @@
 
 function courses_has_visited($pagepath, $tag, $xchan = NULL) {
 	if ($xchan === NULL) $xchan = get_observer_hash();
-	$r=q("select * from coursevisits where coursevisits_xchan = %s and coursevisits_pagepath = %s and coursevisits_tag = %s limit 1",
+	$r=q("select * from coursevisits where coursevisits_xchan = '%s' and coursevisits_pagepath = '%s' and coursevisits_tag = '%s' limit 1",
 		dbesc($xchan),
 		dbesc($pagepath),
 		dbesc($tag)
